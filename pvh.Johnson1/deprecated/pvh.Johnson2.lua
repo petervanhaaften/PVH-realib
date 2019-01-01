580da3f3af636f9bd49e0adfcdd9a33739a2608b
Peter van Haaften <petervh@gmail.com>
2018-12-31 21:30:53 -0500

    johnson transform

---
+++ pvh.Johnson1/deprecated/pvh.Johnson2.lua
@@ -0,0 +1,277 @@
+--[[
+reads note data
+puts each current note into origin buffer
+have another buffer available, new note buffer
+pull out each note from origin buffer, change pitch according to same index in new notes buffer
+insert new note at same position as origin note
+]]--
+
+--[[
+johnson additions
+needs:
+1) function driven processing for unlimited number of sequences
+2) gui w/minimum 8 sequences & origin
+3) ability to start from sequence position number ?
+4) way to visualize/report sequence number at end of midi item ? comment based ??
+]]--
+
+
+function Msg(str)
+  reaper.ShowConsoleMsg(tostring(str) .. "\n")
+end
+-------------------------------
+-- GLOBAL VARIABLES
+-------------------------------
+reaper.ClearConsole();
+
+-- Get window
+window = reaper.MIDIEditor_GetActive()
+
+-- Get current take being edited in MIDI Editor
+take = reaper.MIDIEditor_GetTake(window)
+
+-- Loop through & count each selected note
+retval, notes, ccs, sysex = reaper.MIDI_CountEvts(take)
+i = 0
+check = 0
+notes_selected=0
+
+
+p = {} -- data holder
+p.newnotes = {}
+p.origstartppq = {}
+p.origendppq = {}
+
+p.johnsonseq = {}
+p.johnsonseq_new = {}
+
+-------------------------------
+-- DEBUG FUNCTIONS
+-------------------------------
+-- table serialization from
+-- http://lua-users.org/wiki/TableUtils
+
+function table.val_to_str ( v )
+   if "string" == type( v ) then
+      v = string.gsub( v, "\n", "\\n" )
+      if string.match( string.gsub(v,"[^'\"]",""), '^"+$' ) then
+	 return "'" .. v .. "'"
+      end
+      return '"' .. string.gsub(v,'"', '\\"' ) .. '"'
+   else
+      return "table" == type( v ) and table.tostring( v ) or
+      tostring( v )
+   end
+end
+
+function table.key_to_str ( k )
+   if "string" == type( k ) and string.match( k, "^[_%a][_%a%d]*$" ) then
+      return k
+   else
+      return "[" .. table.val_to_str( k ) .. "]"
+   end
+end
+
+function table.tostring( tbl )
+   local result, done = {}, {}
+   for k, v in ipairs( tbl ) do
+      table.insert( result, table.val_to_str( v ) )
+      done[ k ] = true
+   end
+   for k, v in pairs( tbl ) do
+      if not done[ k ] then
+	 table.insert( result,
+	 table.key_to_str( k ) .. "=" .. table.val_to_str( v ) )
+      end
+   end
+   return "{" .. table.concat( result, "," ) .. "}"
+end
+
+
+      --serialize table
+      --local value = table.tostring(p.johnsonbuf[0])
+
+-------------------------------
+-- FUNCTIONS
+-------------------------------
+function makenoteBuffer()
+   --read GUI values to determine Low & High rand range
+   local low = GUI.Val("Low")
+   local high = GUI.Val("High")
+   local randomMax = math.floor(high - low)
+   --  if randomMax is 0, script will crash, so default to 1
+   if randomMax == 0 then
+      randomMax = 1
+   else end
+   --builds note buffer of equal numnber elements to selected notes
+   for i=0, notes-1 do --loops through all notes in take
+      local random = low + math.random(randomMax) --pitch for note
+      local retval, sel, muted, startppq, endppq, chan, pitch, vel = reaper.MIDI_GetNote(take, i)
+      if sel == true then -- find which notes are selected
+	 p.newnotes[i] = random --fill table with random pitch at each note index
+	 p.origstartppq[i] = startppq --fill table with startppq values at each note index
+	 p.origendppq[i] = endppq --fill table wiuth endppq values at each note index
+      end
+      i=i+1
+   end
+end
+
+
+function getselectedNotes()
+   for i=0, notes-1 do --loops through all notes in take
+      --fill variables with data from each note
+      local retval, sel, muted, startppq, endppq, chan, pitch, vel = reaper.MIDI_GetNote(take, i)
+      if sel == true then -- find which notes are selected
+	 check = check + 1 -- if notes selected add it to check count
+      end
+      i=i+1
+   end
+end
+
+function compareBuffers()
+   for i=0, notes-1 do --loops through all notes in take
+      --fill variables with data from each note
+      local retval, sel, muted, startppq, endppq, chan, pitch, vel = reaper.MIDI_GetNote(take, i) 
+      if sel == true then -- find which notes are selected
+	 reaper.MIDI_InsertNote(take, sel, muted, p.origstartppq[i], p.origendppq[i], chan, p.newnotes[i], vel, false)
+      end
+      --delete eacg original pitch
+      reaper.MIDI_DeleteNote(take, i) 
+      i=i+1
+   end
+end
+
+function johnsonBuffer()
+   for j=0, 7 do
+      p.johnsonseq[j] = {}
+   end
+   --predefine sequences
+   --origin sequence
+   p.johnsonseq_origin = 
+   --new sequences
+   p.johnsonseq[0] = "1102"
+   p.johnsonseq[1] = "010"
+   p.johnsonseq[2] = "0102"
+   p.johnsonseq[3] = "0103"
+   --values from GUI or input text file
+   --test values included here
+   p.johnsonseq_new[0] = "99"
+   p.johnsonseq_new[1] = "88"
+   p.johnsonseq_new[2] = "77"
+   p.johnsonseq_new[3] = "66"
+   for j=0, 3 do --j defines johnsonseq to iterate
+      for k=0,3 do --search each sequence and sub values from johynsonseq_new
+	 p.johnsonseq[j] = string.gsub(p.johnsonseq[j], k, p.johnsonseq_new[k])
+	 k = k+1
+      end
+      Msg("seq" .. j .. " = " .. p.johnsonseq[j])
+      j = j+1
+   end
+   
+   for i=0, notes-1 do --loops through all notes in take
+      --local random = low + math.random(randomMax) --pitch for note
+      local retval, sel, muted, startppq, endppq, chan, pitch, vel = reaper.MIDI_GetNote(take, i)
+      if sel == true then -- find which notes are selected
+	 p.newnotes[i] = random --fill table with random pitch at each note index
+	 p.origstartppq[i] = startppq --fill table with startppq values at each note index
+	 p.origendppq[i] = endppq --fill table wiuth endppq values at each note index
+      end
+      i=i+1
+   end
+   --Msg(p.johnsonbuf)
+end
+
+function makeButton()
+   --when gui 'make' button is pressed. . . 
+   getselectedNotes()
+   makenoteBuffer()
+   compareBuffers()
+   johnsonBuffer()
+end
+
+-------------------------------
+-- GUI
+-------------------------------
+
+local lib_path = reaper.GetExtState("Lokasenna_GUI", "lib_path_v2")
+if not lib_path or lib_path == "" then
+    reaper.MB("Couldn't load the Lokasenna_GUI library. Please run 'Set Lokasenna_GUI v2 library path.lua' in the Lokasenna_GUI folder.", "Whoops!", 0)
+    return
+end
+loadfile(lib_path .. "Core.lua")()
+
+
+
+
+GUI.req("Classes/Class - Textbox.lua")()
+GUI.req("Classes/Class - Button.lua")()
+-- If any of the requested libraries weren't found, abort the script.
+if missing_lib then return 0 end
+
+GUI.name = "pvh.Notemod1"
+GUI.x, GUI.y, GUI.w, GUI.h = 0, 0, 130, 135
+GUI.anchor, GUI.corner = "mouse", "C"
+
+GUI.New("High", "Textbox", {
+    z = 11,
+    x = 48,
+    y = 48,
+    w = 40,
+    h = 20,
+    caption = "High",
+    cap_pos = "left",
+    font_a = 3,
+    font_b = "monospace",
+    color = "txt",
+    bg = "wnd_bg",
+    shadow = true,
+    pad = 4,
+    undo_limit = 20
+})
+
+GUI.New("make", "Button", {
+    z = 11,
+    x = 48,
+    y = 80,
+    w = 48,
+    h = 24,
+    caption = "make",
+    font = 3,
+    col_txt = "txt",
+    col_fill = "elm_frame",
+    --MAIN
+    func = makeButton
+})
+
+GUI.New("Low", "Textbox", {
+    z = 11,
+    x = 48,
+    y = 16,
+    w = 40,
+    h = 20,
+    caption = "Low",
+    cap_pos = "left",
+    font_a = 3,
+    font_b = "monospace",
+    color = "txt",
+    bg = "wnd_bg",
+    shadow = true,
+    pad = 4,
+    undo_limit = 20
+})
+
+GUI.Init()
+GUI.Main()
+
+GUI.Val("Low","60")
+GUI.Val("High","80")
+
+
+-------------------------------
+-- MAIN
+-------------------------------
+
+getselectedNotes()
+makenoteBuffer()
+compareBuffers()
+ johnsonBuffer()
---
+++ pvh.Johnson1/pvh.Johnson3.lua
@@ -0,0 +1,412 @@
+--[[
+reads note data
+puts each current note into origin buffer
+have another buffer available, new note buffer
+pull out each note from origin buffer, change pitch according to same index in new notes buffer
+insert new note at same position as origin note
+]]--
+
+--[[
+johnson additions
+needs:
+1) function driven processing for unlimited number of sequences
+2) gui w/minimum 8 sequences & origin
+3) ability to start from sequence position number ?
+4) way to visualize/report sequence number at end of midi item ? comment based ??
+]]--
+
+
+function Msg(str)
+  reaper.ShowConsoleMsg(tostring(str) .. "\n")
+end
+-------------------------------
+-- GLOBAL VARIABLES
+-------------------------------
+reaper.ClearConsole();
+
+-- Get window
+window = reaper.MIDIEditor_GetActive()
+
+-- Get current take being edited in MIDI Editor
+take = reaper.MIDIEditor_GetTake(window)
+
+-- Loop through & count each selected note
+retval, notes, ccs, sysex = reaper.MIDI_CountEvts(take)
+i = 0
+check = 0
+notes_selected=0
+
+
+p = {} -- data holder
+p.newnotes = {}
+p.origstartppq = {}
+p.origendppq = {}
+
+p.johnsonseq = {}
+p.johnsonseq_new = {}
+
+-------------------------------
+-- DEBUG FUNCTIONS
+-------------------------------
+-- table serialization from
+-- http://lua-users.org/wiki/TableUtils
+
+function table.val_to_str ( v )
+   if "string" == type( v ) then
+      v = string.gsub( v, "\n", "\\n" )
+      if string.match( string.gsub(v,"[^'\"]",""), '^"+$' ) then
+	 return "'" .. v .. "'"
+      end
+      return '"' .. string.gsub(v,'"', '\\"' ) .. '"'
+   else
+      return "table" == type( v ) and table.tostring( v ) or
+      tostring( v )
+   end
+end
+
+function table.key_to_str ( k )
+   if "string" == type( k ) and string.match( k, "^[_%a][_%a%d]*$" ) then
+      return k
+   else
+      return "[" .. table.val_to_str( k ) .. "]"
+   end
+end
+
+function table.tostring( tbl )
+   local result, done = {}, {}
+   for k, v in ipairs( tbl ) do
+      table.insert( result, table.val_to_str( v ) )
+      done[ k ] = true
+   end
+   for k, v in pairs( tbl ) do
+      if not done[ k ] then
+	 table.insert( result,
+	 table.key_to_str( k ) .. "=" .. table.val_to_str( v ) )
+      end
+   end
+   return "{" .. table.concat( result, "," ) .. "}"
+end
+
+
+      --serialize table
+      --local value = table.tostring(p.johnsonbuf[0])
+
+-------------------------------
+-- FUNCTIONS
+-------------------------------
+--[[
+function makenoteBuffer()
+   --read GUI values to determine Low & High rand range
+   --local low = GUI.Val("Low")
+   --local high = GUI.Val("High")
+   --local randomMax = math.floor(high - low)
+   --  if randomMax is 0, script will crash, so default to 1
+   if randomMax == 0 then
+      randomMax = 1
+   else end
+   --builds note buffer of equal numnber elements to selected notes
+   for i=0, notes-1 do --loops through all notes in take
+      local random = low + math.random(randomMax) --pitch for note
+      local retval, sel, muted, startppq, endppq, chan, pitch, vel = reaper.MIDI_GetNote(take, i)
+      if sel == true then -- find which notes are selected
+	 p.newnotes[i] = random --fill table with random pitch at each note index
+	 p.origstartppq[i] = startppq --fill table with startppq values at each note index
+	 p.origendppq[i] = endppq --fill table wiuth endppq values at each note index
+      end
+      i=i+1
+   end
+end
+]]--
+
+function getselectedNotes()
+   for i=0, notes-1 do --loops through all notes in take
+      --fill variables with data from each note
+      local retval, sel, muted, startppq, endppq, chan, pitch, vel = reaper.MIDI_GetNote(take, i)
+      if sel == true then -- find which notes are selected
+	 check = check + 1 -- if notes selected add it to check count
+      end
+      i=i+1
+   end
+end
+
+function compareBuffers()
+   for i=0, notes-1 do --loops through all notes in take
+      --fill variables with data from each note
+      local retval, sel, muted, startppq, endppq, chan, pitch, vel = reaper.MIDI_GetNote(take, i) 
+      if sel == true then -- find which notes are selected
+	 reaper.MIDI_InsertNote(take, sel, muted, p.origstartppq[i], p.origendppq[i], chan, p.newnotes[i], vel, false)
+      end
+      --delete eacg original pitch
+      reaper.MIDI_DeleteNote(take, i) 
+      i=i+1
+   end
+end
+
+function johnsonBuffer()
+   for j=0, 7 do
+      p.johnsonseq[j] = {}
+   end
+   --read gui values
+   p.johnsonseq_origin = GUI.Val("origin")
+   p.johnsonseq_new[0] = GUI.Val("seq0")
+   p.johnsonseq_new[1] = GUI.Val("seq1")
+   p.johnsonseq_new[2] = GUI.Val("seq2")
+   p.johnsonseq_new[3] = GUI.Val("seq3")
+   p.johnsonseq_new[4] = GUI.Val("seq4")
+   p.johnsonseq_new[5] = GUI.Val("seq5")
+   p.johnsonseq_new[6] = GUI.Val("seq6")
+   p.johnsonseq_new[7] = GUI.Val("seq7")
+   --split origin into table
+   p.johnsonseq_origin_new = {}
+   for i = 1, #p.johnsonseq_origin do
+      --writing each element into new origin table, from original origin string
+      p.johnsonseq_origin_new[i] = p.johnsonseq_origin:sub(i, i)
+   end
+   --build table with indexes to be replaced
+   -- & replace values ???
+   p.johnsonseq_index = {}
+   for j = 0, 7 do
+      for k,v in pairs(p.johnsonseq_origin_new) do
+	 jStr = tostring(j) --convert j to string
+	 if v == jStr then
+	    --replace each cell in origin table with appropriate cell in new sequence table
+	    p.johnsonseq_origin_new[k] = p.johnsonseq_new[j]
+	 end
+      end
+      j = j+1
+   end
+   --concatenate transformed origin table
+   p.johnsonseq_origin_final = table.concat(p.johnsonseq_origin_new)
+   Msg(p.johnsonseq_origin_final)
+   --check to see how long sequence is, and if process should continue
+   Msg("numnotes : " .. notes)
+   p.johnsonseq_origin_final_length = string.len(p.johnsonseq_origin_final)
+   Msg("length of origin : " .. p.johnsonseq_origin_final_length)
+   --if final origin length is less than number of selected notes, run process again, else end.
+   if p.johnsonseq_origin_final_length < notes then
+      --move final transformed origin into original origin, for further processing
+      --!!!need to find a way to not pull from GUI the origin in this case !!!
+      p.johnsonseq_origin = p.johnsonseq_origin_final
+      --johnsonBuffer()
+   else end
+end
+
+function makeButton()
+   --when gui 'make' button is pressed. . . 
+   getselectedNotes()
+   --makenoteBuffer()
+   --compareBuffers()
+   johnsonBuffer()
+end
+
+-------------------------------
+-- GUI
+-------------------------------
+-- Script generated by Lokasenna's GUI Builder
+
+
+local lib_path = reaper.GetExtState("Lokasenna_GUI", "lib_path_v2")
+if not lib_path or lib_path == "" then
+    reaper.MB("Couldn't load the Lokasenna_GUI library. Please run 'Set Lokasenna_GUI v2 library path.lua' in the Lokasenna_GUI folder.", "Whoops!", 0)
+    return
+end
+loadfile(lib_path .. "Core.lua")()
+
+
+
+
+GUI.req("Classes/Class - Textbox.lua")()
+GUI.req("Classes/Class - Button.lua")()
+-- If any of the requested libraries weren't found, abort the script.
+if missing_lib then return 0 end
+
+
+
+GUI.name = "pvh.johnsontransform1"
+GUI.x, GUI.y, GUI.w, GUI.h = 0, 0, 215, 335
+GUI.anchor, GUI.corner = "mouse", "C"
+
+
+
+GUI.New("make", "Button", {
+    z = 11,
+    x = 48,
+    y = 304,
+    w = 48,
+    h = 24,
+    caption = "make",
+    font = 3,
+    col_txt = "txt",
+    col_fill = "elm_frame",
+    func = makeButton
+})
+GUI.New("seq2", "Textbox", {
+    z = 11,
+    x = 48,
+    y = 112,
+    w = 148,
+    h = 20,
+    caption = "seq2",
+    cap_pos = "left",
+    font_a = 3,
+    font_b = "monospace",
+    color = "txt",
+    bg = "wnd_bg",
+    shadow = true,
+    pad = 4,
+    undo_limit = 20
+})
+
+GUI.New("seq3", "Textbox", {
+    z = 11,
+    x = 48,
+    y = 144,
+    w = 148,
+    h = 20,
+    caption = "seq3",
+    cap_pos = "left",
+    font_a = 3,
+    font_b = "monospace",
+    color = "txt",
+    bg = "wnd_bg",
+    shadow = true,
+    pad = 4,
+    undo_limit = 20
+})
+
+GUI.New("seq6", "Textbox", {
+    z = 11,
+    x = 48,
+    y = 240,
+    w = 148,
+    h = 20,
+    caption = "seq6",
+    cap_pos = "left",
+    font_a = 3,
+    font_b = "monospace",
+    color = "txt",
+    bg = "wnd_bg",
+    shadow = true,
+    pad = 4,
+    undo_limit = 20
+})
+
+GUI.New("seq7", "Textbox", {
+    z = 11,
+    x = 48,
+    y = 272,
+    w = 148,
+    h = 20,
+    caption = "seq7",
+    cap_pos = "left",
+    font_a = 3,
+    font_b = "monospace",
+    color = "txt",
+    bg = "wnd_bg",
+    shadow = true,
+    pad = 4,
+    undo_limit = 20
+})
+
+GUI.New("seq4", "Textbox", {
+    z = 11,
+    x = 48,
+    y = 176,
+    w = 148,
+    h = 20,
+    caption = "seq4",
+    cap_pos = "left",
+    font_a = 3,
+    font_b = "monospace",
+    color = "txt",
+    bg = "wnd_bg",
+    shadow = true,
+    pad = 4,
+    undo_limit = 20
+})
+
+GUI.New("seq5", "Textbox", {
+    z = 11,
+    x = 48,
+    y = 208,
+    w = 148,
+    h = 20,
+    caption = "seq5",
+    cap_pos = "left",
+    font_a = 3,
+    font_b = "monospace",
+    color = "txt",
+    bg = "wnd_bg",
+    shadow = true,
+    pad = 4,
+    undo_limit = 20
+})
+
+GUI.New("seq0", "Textbox", {
+    z = 11,
+    x = 48,
+    y = 48,
+    w = 148,
+    h = 20,
+    caption = "seq0",
+    cap_pos = "left",
+    font_a = 3,
+    font_b = "monospace",
+    color = "txt",
+    bg = "wnd_bg",
+    shadow = true,
+    pad = 4,
+    undo_limit = 20
+})
+
+GUI.New("origin", "Textbox", {
+    z = 11,
+    x = 48,
+    y = 16,
+    w = 148,
+    h = 20,
+    caption = "origin",
+    cap_pos = "left",
+    font_a = 3,
+    font_b = "monospace",
+    color = "txt",
+    bg = "wnd_bg",
+    shadow = true,
+    pad = 4,
+    undo_limit = 20
+})
+
+GUI.New("seq1", "Textbox", {
+    z = 11,
+    x = 48,
+    y = 80,
+    w = 148,
+    h = 20,
+    caption = "seq1",
+    cap_pos = "left",
+    font_a = 3,
+    font_b = "monospace",
+    color = "txt",
+    bg = "wnd_bg",
+    shadow = true,
+    pad = 4,
+    undo_limit = 20
+})
+
+GUI.Init()
+GUI.Main()
+
+
+GUI.Val("origin","01101")
+GUI.Val("seq0","100")
+GUI.Val("seq1","010")
+
+
+-------------------------------
+-- MAIN
+-------------------------------
+
+--getselectedNotes()
+--makenoteBuffer()
+--compareBuffers()
+--johnsonBuffer()
